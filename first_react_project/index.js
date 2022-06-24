e03c8a797b962b763e596a95c33c98f82e7ef084
Tanveer Ahmed <tanveerahmed2619@gmail.com>
2022-06-24 19:48:07 +0530

    update: index.js

--- first_react_project/index.js
+++ first_react_project/index.js
@@ -1,21 +1,43 @@
-function Page() {
+// Header component
+function Header() {
+    return (
+        <header>
+            <nav>
+                <img src="react_logo.png" width="40px"></img>
+            </nav>
+        </header>
+    )
+}
+
+// Footer component
+function Footer() {
+    return (
+        <footer>
+            <small>&copy; 2022 Ahmed development. All rights reserved.</small>
+        </footer>
+    )
+}
+
+// MainContent component
+function MainContent() {
     return (
         <div>
-            <header>
-                <nav>
-                    <img src="react_logo.png" width="40px"></img>
-                </nav>
-            </header>
             <h1>Why React?</h1>
             <ol>
                 <li>It's a popular library.</li>
                 <li>It's composable</li>
                 <li>It's declarative</li>
             </ol>
+        </div>
+    )
+}
 
-            <footer>
-                <small>"&copy; 2022 Ahmed development. All rights reserved."</small>
-            </footer>
+function Page() {
+    return (
+        <div>
+            <Header />
+            <MainContent />
+            <Footer />
         </div>
     )
 }
