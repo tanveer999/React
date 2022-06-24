cf637ffdfe41c70e5bd6e016de270c499053c180
Tanveer Ahmed <tanveerahmed2619@gmail.com>
2022-06-24 21:20:18 +0530

    add: first_react_app

---
+++ first_react_app/src/Footer.js
@@ -0,0 +1,10 @@
+// Footer component
+function Footer() {
+    return (
+        <footer className="footer">
+            &copy; 2022 Ahmed development. All rights reserved.
+        </footer>
+    )
+}
+
+export default Footer
---
+++ first_react_app/src/MainContent.js
@@ -0,0 +1,15 @@
+// MainContent component
+function MainContent() {
+    return (
+        <div>
+            <h1>Why React?</h1>
+            <ol>
+                <li>It's a popular library.</li>
+                <li>It's composable</li>
+                <li>It's declarative</li>
+            </ol>
+        </div>
+    )
+}
+
+export default MainContent
