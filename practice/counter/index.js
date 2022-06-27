34a33ae7894c1225e3b096ec5b4b1e0deca92a9b
Tanveer Ahmed <tanveerahmed2619@gmail.com>
2022-06-27 20:38:20 +0530

    update: counter.js

--- practice/counter/index.js
+++ practice/counter/index.js
@@ -2,8 +2,28 @@ function App() {
 
     const [count, setCount] = React.useState(0)
 
+    // function decreaseCount() {
+    //     setCount(count - 1)
+    // }
+    // function decreaseCount() {
+    //     setCount(function (oldValue) {
+    //         return oldValue - 1
+    //     })
+    // }
+     /**
+     * Note: if you ever need the old value of state
+     * to help you determine the new value of state,
+     * you should pass a callback function to your
+     * state setter function instead of using
+     * state directly. This callback function will
+     * receive the old value of state as its parameter,
+     * which you can then use to determine your new
+     * value of state.
+     */
     function decreaseCount() {
-        setCount(count - 1)
+        setCount(prevCount => {
+            return prevCount - 1
+        })
     }
 
     function increaseCount() {
