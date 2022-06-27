3040b08da77ba7adebe93b4694e40f397bdedf80
Tanveer Ahmed <tanveerahmed2619@gmail.com>
2022-06-27 20:30:48 +0530

    add: counter

--- meme-generator/src/component/Meme.js
+++ meme-generator/src/component/Meme.js
@@ -1,11 +1,26 @@
+import memesData from "../memesData"
+
 function Meme() {
+
+    function getMemeImage() {
+        const memesArray = memesData.data.memes
+        const index = Math.floor(Math.random() * memesArray.length)
+        const url = memesArray[index].url
+        console.log(url)
+    }
+    
     return (
     <main>
-        <form className="form">
+        <div className="form">
             <input type="text" placeholder="Top text" className="form--input"></input>
             <input type="text" placeholder="Bottom text" className="form--input"></input>
-            <button className="form--button">Get a new meme image  🖼</button>
-        </form>
+            <button 
+            className="form--button"
+            onClick={getMemeImage}
+            >
+                Get a new meme image  🖼
+            </button>
+        </div>
     </main>
     )
 }
