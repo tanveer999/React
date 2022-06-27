28b77e14ca1592c57860778111fdcf7f885e9119
Tanveer Ahmed <tanveerahmed2619@gmail.com>
2022-06-27 15:36:23 +0530

    update: using javascript spread feature

--- airbnb_clone/src/App.js
+++ airbnb_clone/src/App.js
@@ -3,12 +3,14 @@ import Hero from "./components/Hero"
 import Card from "./components/Card" 
 import CardData from "./data"
 
+
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Spread_syntax#spread_in_object_literals
 function App() {
 
   const cards = CardData.map((item) => {
     return <Card 
       key={item.id}
-      item={item}
+      {...item}
     />
   })
 
