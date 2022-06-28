7db1f4a3330a1dedf1fa5aeb8058fa5860b1b4d0
Tanveer Ahmed <tanveerahmed2619@gmail.com>
2022-06-28 16:05:42 +0530

    update: using object state for memedata

--- meme-generator/src/component/Meme.js
+++ meme-generator/src/component/Meme.js
@@ -1,12 +1,27 @@
 import memesData from "../memesData"
-
+import React, { useState } from 'react';
 function Meme() {
+    
+    // const [imageUrl, setImageUrl] = useState('http://i.imgflip.com/1bij.jpg')
+    const [meme, setMeme] = useState({
+        topText: "",
+        bottomText: "",
+        radomImage: "http://i.imgflip.com/1bij.jpg"
+    })
+
+    const [allMemeImages, setAllmeme] = useState(memesData)
 
     function getMemeImage() {
-        const memesArray = memesData.data.memes
+        const memesArray = allMemeImages.data.memes
         const index = Math.floor(Math.random() * memesArray.length)
         const url = memesArray[index].url
-        console.log(url)
+        // console.log(url)
+        setMeme(prevMeme => {
+            return {
+            ...prevMeme,
+            radomImage: url
+            }
+        })
     }
     
     return (
@@ -21,6 +36,7 @@ function Meme() {
                 Get a new meme image  🖼
             </button>
         </div>
+        <img src={meme.radomImage} alt="meme" className="meme--image"></img>
     </main>
     )
 }
