97d4847ed4888a3f95fc726b8047cbe9700464c2
Tanveer Ahmed <tanveerahmed2619@gmail.com>
2022-06-29 21:45:14 +0530

    add: radio buttons

--- practice/practice_form/src/App.js
+++ practice/practice_form/src/App.js
@@ -5,20 +5,25 @@ function App() {
 
   const [formData, setFormData] = useState(
     {
-      firstName: "", lastName: "", comments: ""
+      firstName: "", 
+      lastName: "", 
+      comments: "",
+      isFriendly: true,
+      employment: ""
     }
   )
   console.log(formData)
   // event is passed by the browser and it has lot of information
   // target returns dom element which triggered the event
   function handleChange(event) {
-    // console.log(event.target.value)
+    
+    const {name, value, type, checked} = event.target
 
     setFormData(prevFormData => {
       //[event.target.name] when returning dynamic values enclose with []
       return {
         ...prevFormData,
-        [event.target.name]: event.target.value
+        [name] : type === "checkbox" ? checked : value
       }
     })
   }
@@ -45,8 +50,57 @@ function App() {
       placeholder="comments"
       onChange={handleChange} 
       value={formData.comments} />
+      <br></br>
+
+      <input
+      type="checkbox"
+      name="isFriendly"
+      id="isFriendly"
+      checked={formData.isFriendly}
+      onChange={handleChange}
+      />
+      <label htmlFor="isFriendly">Are you friendly?</label>
+      <br></br>
+      <br></br>
+      <fieldset>
+                <legend>Current employment status</legend>
+                
+                <input 
+                    type="radio"
+                    id="unemployed"
+                    name="employment"
+                    value="unemployed"
+                    checked={formData.employment === "unemployed"}
+                    onChange={handleChange}
+                />
+                <label htmlFor="unemployed">Unemployed</label>
+                <br />
+                
+                <input 
+                    type="radio"
+                    id="part-time"
+                    name="employment"
+                    value="part-time"
+                    checked={formData.employment === "part-time"}
+                    onChange={handleChange}
+                />
+                <label htmlFor="part-time">Part-time</label>
+                <br />
+                
+                <input 
+                    type="radio"
+                    id="full-time"
+                    name="employment"
+                    value="full-time"
+                    checked={formData.employment === "full-time"}
+                    onChange={handleChange}
+                />
+                <label htmlFor="full-time">Full-time</label>
+                <br />
+                
+            </fieldset>
     </form>
   );
 }
-
+// state and dom name value should be same
 export default App;
