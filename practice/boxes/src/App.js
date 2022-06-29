90d195e36e7f1a9ec79b487fdbab8bee2c04ab5b
Tanveer Ahmed <tanveerahmed2619@gmail.com>
2022-06-29 18:39:00 +0530

    update: add toggle

--- practice/boxes/src/App.js
+++ practice/boxes/src/App.js
@@ -4,13 +4,30 @@ import Box from "./Box"
 
 function App(props) {
   // console.log(boxes)
-
-  function toggle(id) {
-    console.log(id)
-  }
    
   const [squares, setSquares] = useState(boxes)
   console.log("App component")
+
+  function toggle(id) {
+      console.log(id)
+      setSquares(prevSquares => {
+        const newSquares = []
+        for(let i = 0; i < prevSquares.length; i++) {
+            const currentSquare = prevSquares[i]
+            if(currentSquare.id === id) {
+                const updatedSquare = {
+                    ...currentSquare,
+                    on: !currentSquare.on
+                }
+                newSquares.push(updatedSquare)
+            } else {
+                newSquares.push(currentSquare)
+            }
+        }
+        return newSquares
+    })
+  }
+
   const squareElements = squares.map(square => (
     <Box on={square.on} key={square.id} id={square.id} toggle={toggle}/>
   ))
--- practice/boxes/src/Box.js
+++ practice/boxes/src/Box.js
@@ -2,17 +2,20 @@ import {useState} from "react"
 function Box(props) {
 
     // using props to create state is called as derived state
-    const [colorOn, setColorOn] = useState(props.on)
+    // const [colorOn, setColorOn] = useState(props.on)
 
-    function toggleColor() {
-        setColorOn(prevColorOn => {
-           return !prevColorOn
-        }) 
-    }
+    // function toggleColor() {
+    //     setColorOn(prevColorOn => {
+    //        return !prevColorOn
+    //     }) 
+    // }
 
     const style = {
-        backgroundColor: colorOn? "black" : "transparent"
+        backgroundColor: props.on? "black" : "transparent"
     }
+    console.log("box")
+    console.log(props.id)
+
     console.log("Box component")
     return (
         <div style={style} className="box" onClick={() => props.toggle(props.id)}></div>
