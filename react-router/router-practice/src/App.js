59f96a1e71307e4908243537827631d5cc27f1bf
Tanveer Ahmed <tanveerahmed2619@gmail.com>
2022-07-01 15:56:54 +0530

    add: error page and navbar

--- react-router/router-practice/src/App.js
+++ react-router/router-practice/src/App.js
@@ -3,15 +3,19 @@ import {BrowserRouter, Routes, Route} from "react-router-dom"
 import Home from "./components/Home"
 import About from "./components/About"
 import Products from "./components/Products"
+import Error from "./components/Error"
 
 function App() {
   return (
     <BrowserRouter>
+      <nav>our navbar</nav>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/products" element={<Products />} />
+        <Route path="*" element={<Error />} />
       </Routes>
+      <footer>our footer</footer>
     </BrowserRouter>
   )
 }
